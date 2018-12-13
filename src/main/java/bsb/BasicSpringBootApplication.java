4e16edd5808278f273bf8f477f14d2aaebd2597e
master-d <goodeveningthisisrobrichards@gmail.com>
2018-12-12 23:31:52 -0600

    working copy

--- src/main/java/bsb/BasicSpringBootApplication.java
+++ src/main/java/bsb/BasicSpringBootApplication.java
@@ -1,4 +1,4 @@
-package bsb.basicspringboot;
+package bsb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/bsb/controller/BasicController.java
+++ src/main/java/bsb/controller/BasicController.java
@@ -1,13 +1,14 @@
 package bsb.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class BasicController {
 
-	@RequestMapping("/")
-	public String index() {
-		return "goodevening";
+	@RequestMapping(value="/*",produces="text/plain")
+	public String index(@RequestParam(name="name", defaultValue="sir") String name) {
+		return "goodevening " + name;
 	}
 }
