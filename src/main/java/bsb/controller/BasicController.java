2a8d1454f36aec47ddb2c4d51b9976d9d9a696bf
master-d <goodeveningthisisrobrichards@gmail.com>
2018-12-12 22:27:13 -0600

    ff

---
+++ src/main/java/bsb/controller/BasicController.java
@@ -0,0 +1,13 @@
+package bsb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BasicController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "goodevening";
+	}
+}
