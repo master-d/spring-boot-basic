6d8282b857948052672fcad51f81ada6b30c0b42
master-d <goodeveningthisisrobrichards@gmail.com>
2019-01-06 16:36:47 -0600

    --modified controller so it returns html files

--- src/main/java/bsb/BasicSpringBootApplication.java
+++ src/main/java/bsb/BasicSpringBootApplication.java
@@ -2,8 +2,12 @@ package bsb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication
+@ComponentScan(basePackages= {"bsb","bsb.controllers"})
 public class BasicSpringBootApplication {
 
 	public static void main(String[] args) {
--- src/main/java/bsb/controller/BasicController.java
+++ src/main/java/bsb/controller/BasicController.java
@@ -1,14 +1,15 @@
 package bsb.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class BasicController {
 
-	@RequestMapping(value="/*",produces="text/plain")
+	@RequestMapping(value="/",produces="text/html")
 	public String index(@RequestParam(name="name", defaultValue="sir") String name) {
-		return "goodevening " + name;
+		return "goodevening";
 	}
 }
