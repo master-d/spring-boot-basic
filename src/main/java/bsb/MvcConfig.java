268792b9c013643e4b73209f716adcf1ea294646
master-d <goodeveningthisisrobrichards@gmail.com>
2019-01-06 16:41:54 -0600

    --added goodevening.html

---
+++ src/main/java/bsb/MvcConfig.java
@@ -0,0 +1,18 @@
+package bsb;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class MvcConfig {
+/*
+	@Bean
+	InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver irvr = new InternalResourceViewResolver();
+		//irvr.setPrefix("/static/");
+		irvr.setSuffix(".html");
+		return irvr;
+	}
+*/	
+}
